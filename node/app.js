9121022fdc5af76e566017f0d1a05b26dacbcd45
Keanan Koppenhaver <keanan@alphaparticle.com>
2022-10-25 02:15:23 -0500

    Update with ability to send notifications

--- node/app.js
+++ node/app.js
@@ -3,14 +3,38 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const OneSignal = require('onesignal-node');
+
 app.use(express.json());
 
 app.use(express.static("express"));
 
 // default URL for website
-app.use('/', function(req,res){
+app.get('/', function(req,res){
   res.sendFile(path.join(__dirname+'/express/index.html'));
   //__dirname : It will resolve to your project folder.
+});
+
+app.get('/notify', function(req,res){
+  // Update your APP_ID and API_KEY from OneSignal here
+  const client = new OneSignal.Client(APP_ID, API_KEY);
+
+  // This is where we will make our call to OneSignal
+  const notification = {
+    headings: {
+      'en': 'Hello from OneSignal!'
+    },
+    contents: {
+      'en': 'This is a notification triggered from our Node app and pushed into React Native.',
+    },
+    included_segments: ['Subscribed Users']
+  };
+
+  client.createNotification(notification)
+  .then(response => {console.log(response)})
+  .catch(e => {console.log(e)});
+
+  res.send('Notification request sent to OneSignal.');
 });
 
 const server = http.createServer(app);
